bf18eba831392f36a85af14eacd1eed786d3cd8d
Rok Ajdnik <r.ajdnik@gmail.com>
2019-01-11 07:59:14 +0100

    Battery style update
    Updated battery widget background colors to reflect various
    statuses such as charging and battery low.

--- lib/Battery/index.jsx
+++ lib/Battery/index.jsx
@@ -16,14 +16,38 @@ const displayIcon = (batteryPercentage, isCharging) => {
   }
 }
 
+const updateStyling = (batteryPercentage, isCharging) => {
+  let contentStyle = JSON.parse(JSON.stringify(content));
+  let arrowStyle = JSON.parse(JSON.stringify(arrow));
+  if (isCharging === true) {
+    contentStyle.background = 'rgba(163, 189, 140, 1)';
+    contentStyle.color = 'rgba(76, 86, 106, 1)';
+    arrowStyle.borderRight = '10px solid rgba(163, 189, 140, 1)';
+  } else if (batteryPercentage < 60) {
+    contentStyle.background = 'rgba(235, 203, 139, 1)';
+    contentStyle.color = 'rgba(76, 86, 106, 1)';
+    arrowStyle.borderRight = '10px solid rgba(235, 203, 139, 1)';
+  } else if (batteryPercentage < 40) {
+    contentStyle.background = 'rgba(208, 135, 113, 1)';
+    contentStyle.color = 'rgba(76, 86, 106, 1)';
+    arrowStyle.borderRight = '10px solid rgba(208, 135, 113, 1)';
+  } else if (batteryPercentage < 20) {
+    contentStyle.background = 'rgba(191, 97, 106, 1)';
+    contentStyle.color = 'rgba(76, 86, 106, 1)';
+    arrowStyle.borderRight = '10px solid rgba(191, 97, 106, 1)';
+  }
+  return { contentStyle, arrowStyle };
+}
+
 const render = ({output}) => {
   if (typeof output === 'undefined') return null;
   const batteryIcon = displayIcon(output.percentage, output.charging);
   const iconClasses = `fas ${batteryIcon}`;
+  const { contentStyle, arrowStyle } = updateStyling(output.percentage, output.charging);
   return (
     <div style={container}>
-      <div style={arrow}/>
-      <div style={content}>
+      <div style={arrowStyle}/>
+      <div style={contentStyle}>
         <i class={iconClasses}/>&nbsp;{output.percentage}%
       </div>
     </div>
