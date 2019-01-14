3d57f07eb42a407b445c0955b7a1b557c4956661
Rok Ajdnik <r.ajdnik@gmail.com>
2019-01-14 09:35:35 +0100

    Multiple monitor support
    Changed the desktop widget logic to support multiple monitors.

--- lib/Desktop/index.jsx
+++ lib/Desktop/index.jsx
@@ -1,13 +1,13 @@
 import { container, arrow, arrowLight, content } from './style.jsx';
 
-const renderDesktop = (number, active) => {
+const renderDesktop = (index, desktop, active) => {
   let contentStyle = JSON.parse(JSON.stringify(content));
-	contentStyle.left = (number * 35 - 45) + 'px';
+	contentStyle.left = (index * 35 - 45) + 'px';
   let arrowStyle = JSON.parse(JSON.stringify(arrow));
-	arrowStyle.left = (number * 35 - 10) + 'px';
+	arrowStyle.left = (index * 35 - 10) + 'px';
   let arrowLightStyle = JSON.parse(JSON.stringify(arrowLight));
-	arrowLightStyle.left = (number * 35 - 9) + 'px';
-	if (number === active) {
+	arrowLightStyle.left = (index * 35 - 9) + 'px';
+	if (desktop === active) {
 		contentStyle.background = 'rgba(235, 239, 243, 1)';
 		contentStyle.color = 'rgba(76, 86, 106, 1)';
 		arrowStyle.borderLeft = '10px solid rgba(235, 239, 243, 1)';
@@ -15,7 +15,7 @@ const renderDesktop = (number, active) => {
   return (
       <span>
         <div style={contentStyle}>
-        {number}
+        {desktop}
         </div>
         <div style={arrowLightStyle}/>
         <div style={arrowStyle}/>
@@ -27,8 +27,8 @@ const render = ({output}) => {
   if (typeof output === 'undefined') return null;
   
   const desktops = [];
-  for (let num = output.total; num > 0; --num) {
-    desktops.push(renderDesktop(num, output.active));
+  for (let num = output.end; num >= output.start; --num) {
+    desktops.push(renderDesktop(num - output.start + 1, num, output.active));
   }
   
   return (
--- status-right.sh
+++ status-right.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+CURRENT_DESKTOP=$(chunkc tiling:query -m id)
+
+
 DESKTOP_ACTIVE=$(chunkc tiling::query -d id)
-DESKTOP_TOTAL=$(chunkc tiling::query -D 1 | tail -c 1)
+DESKTOP_START=$(chunkc tiling::query -D $CURRENT_DESKTOP | head -c 1)
+DESKTOP_END=$(chunkc tiling::query -D $CURRENT_DESKTOP | tail -c 1)
 
 echo $(cat <<-EOF
 {
 	"desktop": {
 		"active": $DESKTOP_ACTIVE,
-		"total": $DESKTOP_TOTAL
+		"start": $DESKTOP_START,
+    "end": $DESKTOP_END
 	}
 }
 EOF
