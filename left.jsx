3a2d453b79397de5ed0a55fc8d67d98ced7e6f91
Rok Ajdnik <r.ajdnik@gmail.com>
2019-06-09 14:00:33 +0200

    fix: mixed up naming

--- left.jsx
+++ left.jsx
@@ -1,26 +1,16 @@
-import DateTime from './lib/DateTime/index.jsx';
-import Battery from './lib/Battery/index.jsx';
-import Cpu from './lib/Cpu/index.jsx';
-import Memory from './lib/Memory/index.jsx';
-import Hdd from './lib/Hdd/index.jsx';
-import Wifi from './lib/Wifi/index.jsx';
-import { leftSide } from './lib/style.jsx';
+import Desktop from './lib/Desktop/index.jsx';
+import { rightSide } from './lib/style.jsx';
 import parse from './lib/parse.jsx';
 
-export const refreshFrequency = 15000
+export const refreshFrequency = 500
 
 export const command = './powerbar/status-left.sh'
 
 export const render = ({output}) => {
   const data = parse(output);
   return (
-    <div style={leftSide}>
-			<Wifi output={data.wifi}/>
-			<Hdd output={data.hdd}/>
-			<Memory output={data.memory}/>
-			<Cpu output={data.cpu}/>
-      <Battery output={data.battery}/>
-      <DateTime output={data.datetime}/>
+    <div style={rightSide}>
+			<Desktop output={data.desktop}/>
     </div>
   )
 }
--- status-right.sh
+++ status-right.sh
@@ -1,18 +1,60 @@
 #!/bin/bash
 
-CURRENT_DESKTOP=$(chunkc tiling:query -m id)
+export LC_TIME="en_US.UTF-8"
+TIME=$(date +"%H:%M")
+DATE=$(date +"%a %d/%m/%Y")
 
+BATTERY_PERCENTAGE=$(pmset -g batt | egrep '([0-9]+\%).*' -o --colour=auto | cut -f1 -d'%')
 
-DESKTOP_ACTIVE=$(chunkc tiling::query -d id)
-DESKTOP_START=$(chunkc tiling::query -D $CURRENT_DESKTOP | head -c 1)
-DESKTOP_END=$(chunkc tiling::query -D $CURRENT_DESKTOP | tail -c 1)
+BATTERY_STATUS=$(pmset -g batt | grep "'.*'" | sed "s/'//g" | cut -c 18-19)
+
+BATTERY_CHARGING=""
+if [ "$BATTERY_STATUS" == "Ba" ]; then
+  BATTERY_CHARGING="false"
+elif [ "$BATTERY_STATUS" == "AC" ]; then
+  BATTERY_CHARGING="true"
+fi
+
+LOAD_AVERAGE=$(sysctl -n vm.loadavg | awk '{print $2}')
+
+VOLUME=$(osascript -e 'output volume of (get volume settings)')
+IS_MUTED=$(osascript -e 'output muted of (get volume settings)')
+
+HDD_TOTAL_BYTES=$(df | grep -m 1 /disk1 | awk -F" " '{print $2}')
+HDD_FREE_BYTES=$(df | grep -m 1 /disk1 | awk -F" " '{print $4}')
+
+MEMORY_FREE=$(memory_pressure | grep "Pages free" | grep -o -E '[0-9]+')
+MEMORY_TOTAL=$(memory_pressure | grep system | awk -F" " '{print $5}' | grep -o -E '[0-9]+')
+
+WIFI_SSID=$(networksetup -getairportnetwork en0 | cut -c 24-)
 
 echo $(cat <<-EOF
 {
-	"desktop": {
-		"active": $DESKTOP_ACTIVE,
-		"start": $DESKTOP_START,
-    "end": $DESKTOP_END
+  "datetime": {
+    "time": "$TIME",
+    "date": "$DATE"
+  },
+  "battery": {
+    "percentage": $BATTERY_PERCENTAGE,
+    "charging": $BATTERY_CHARGING
+  },
+  "cpu": {
+    "loadAverage": $LOAD_AVERAGE
+  },
+	"volume": {
+		"volume": $VOLUME,
+		"muted": $IS_MUTED
+	},
+	"hdd": {
+		"freeBytes": $HDD_FREE_BYTES,
+		"totalBytes": $HDD_TOTAL_BYTES
+	},
+	"memory": {
+		"total": $MEMORY_TOTAL,
+		"free": $MEMORY_FREE
+	},
+	"wifi": {
+		"ssid": "$WIFI_SSID"
 	}
 }
 EOF
