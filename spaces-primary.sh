0ba42a84050022bcefbcee3adcb800cd081c9120
Gadzhi Kharkharov <me@kkga.me>
2019-07-04 00:13:37 +0300

    rename files

---
+++ spaces-primary.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PATH=/usr/local/bin/:$PATH
+
+# Check if chunkc exists
+if ! [ -x "$(command -v yabai)" ]; then
+  echo "{\"error\":\"yabai binary not found\"}"
+  exit 1
+fi
+
+SPACES=$(yabai -m query --spaces --display 1)
+ACTIVE=$(yabai -m query --spaces --space | jq .index)
+APP_NAME=$(yabai -m query --windows --window | jq .app)
+
+echo $(cat <<-EOF
+{
+	"desktop": {
+        "spaces": $SPACES,
+        "active": $ACTIVE,
+        "app": $APP_NAME
+	}
+}
+EOF
+)
---
+++ stats.jsx
@@ -0,0 +1,37 @@
+import DateTime from "./lib/DateTime/index.jsx";
+import Battery from "./lib/Battery/index.jsx";
+import Cpu from "./lib/Cpu/index.jsx";
+import Memory from "./lib/Memory/index.jsx";
+import Hdd from "./lib/Hdd/index.jsx";
+import Wifi from "./lib/Wifi/index.jsx";
+import Error from "./lib/Error/index.jsx";
+import { rightSide } from "./lib/style.jsx";
+import parse from "./lib/parse.jsx";
+
+export const refreshFrequency = 15000;
+
+export const command = "./nibar/stats.sh";
+
+export const render = ({ output }) => {
+  console.log(`Right bar output: ${output}`);
+  const data = parse(output);
+  if (typeof data === "undefined") {
+    return (
+      <div style={rightSide}>
+        <Error msg="Error: unknown script output" side="right" />
+      </div>
+    );
+  }
+  return (
+    <div style={rightSide}>
+      <Wifi output={data.wifi} />
+      {/* <Hdd output={data.hdd} /> */}
+      {/* <Memory output={data.memory} /> */}
+      <Cpu output={data.cpu} />
+      <Battery output={data.battery} />
+      <DateTime output={data.datetime} />
+    </div>
+  );
+};
+
+export default null;
