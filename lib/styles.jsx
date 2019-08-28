dbbb95f949b38bf7f0eb67c46ec457f382f7575f
Gadzhi Kharkharov <me@kkga.me>
2019-08-28 20:53:49 +0300

    move colors and fonts into separate file

---
+++ lib/styles.jsx
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    fg: "#bcbcbc",
+    dim: "#6c6c6c",
+    bg: "#262626",
+    red: "#ff8700"
+  },
+  fontSize: "15px",
+  lineHeight: "20px",
+  fontFamily: "Sudo"
+};
