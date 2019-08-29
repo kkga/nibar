8ef7aa727321f5194de686a98948d2522c1c3c28
Gadzhi Kharkharov <me@kkga.me>
2019-08-29 13:25:35 +0300

    fix battery widget and yabai for secondary display

--- lib/Battery.jsx
+++ lib/Battery.jsx
@@ -3,11 +3,15 @@ import styles from "./styles.jsx";
 const render = ({ output }) => {
   return (
     <div>
-      <div>
+      <div
+        style={
+          output.percentage < 20 && output.charging == false
+            ? { color: styles.colors.red }
+            : null
+        }
+      >
         <span>{output.charging ? "+" : " "}</span>
-        <span
-          style={output.percentage < 20 ? { color: styles.colors.red } : null}
-        >
+        <span>
           {output.percentage}% ({output.remaining})
         </span>
       </div>
