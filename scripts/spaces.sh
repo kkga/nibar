ce380ba27b499450b729989eda82f9223405188f
Gadzhi Kharkharov <me@kkga.me>
2019-11-20 14:29:05 +0300

    add new spaces script

---
+++ scripts/spaces.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+PATH=/usr/local/bin/:$PATH
+
+# Check if yabai exists
+if ! [ -x "$(command -v yabai)" ]; then
+  echo "{\"error\":\"yabai binary not found\"}"
+  exit 1
+fi
+
+SPACES_PRIMARY=$(yabai -m query --spaces --display 1)
+SPACES_SECONDARY=$(yabai -m query --spaces --display 2)
+
+echo $(cat <<-EOF
+{
+  "spaces_primary": $SPACES_PRIMARY,
+  "spaces_secondary": $SPACES_SECONDARY
+}
+EOF
+)
